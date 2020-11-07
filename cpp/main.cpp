44542f11d4cc51d8d5d7ad5180ac4b7548c94fa8
Sang-geon Yun <c0510gy@protonmail.ch>
2020-11-07 21:45:16 +0900

    feat: add training test

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -32,6 +32,7 @@ int main(){
   vector<int> O1{0, 0, 0};
   vector<int> O2{1, 1, 1};
   vector<int> O3{0, 1, 1, 1, 1, 1};
+  cout << "Before any train: " << endl;
   cout << hmm.getLikelihood(O1) << endl;
   cout << hmm.getLikelihood(O2) << endl;
   cout << hmm.getLikelihood(O3) << endl;
@@ -39,5 +40,17 @@ int main(){
   cout << hmm.decode(O1) << endl;
   cout << hmm.decode(O2) << endl;
   cout << hmm.decode(O3) << endl;
+
+  for(int j = 0; j < 10; ++j){
+    cout << "training HMM for O3 " << j << endl;
+    hmm.train(O3);
+    cout << hmm.getLikelihood(O1) << endl;
+    cout << hmm.getLikelihood(O2) << endl;
+    cout << hmm.getLikelihood(O3) << endl;
+
+    cout << hmm.decode(O1) << endl;
+    cout << hmm.decode(O2) << endl;
+    cout << hmm.decode(O3) << endl;
+  }
   return 0;
 }
